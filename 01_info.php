1892774436501c8958ef724330a6ad21df64ac0a
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-08-17 22:27:48 +0530

    Revise Php concept

---
+++ 01_info.php
@@ -0,0 +1,23 @@
+<?php
+echo "hello <br>";
+echo "bye";
+// this is comment
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<h1>Intoduction to php</h1>
+
+<body>
+
+</body>
+
+</html>
