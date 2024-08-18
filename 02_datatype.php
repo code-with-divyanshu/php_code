d472cffbf623e486582714a49c05fa9e6ed2c23c
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-08-18 20:13:16 +0530

    Datatype in Php

---
+++ 02_datatype.php
@@ -0,0 +1,35 @@
+<?php
+// string datatype
+$name = "Karan";
+$fruit = "Mango";
+
+
+echo "Hello {$name} <br>";
+echo "You like {$fruit} <br>";
+
+// Integer datatype
+
+$age = 21;
+
+echo "You are {$age} years old <br>";
+
+// Float/double datatype
+
+$cgpa = 7.7;
+$price = 4.54;
+
+echo "Your cgpa is: {$cgpa}<br> ";
+echo "Your pizza is: \${$price}<br> ";
+
+
+// Boolean datatype
+
+$employed = true;
+$isOnline = false;
+
+echo "Rohit is: {$employed}<br>";
+echo "Rohit is: {$isOnline}<br>";
+
+// print two variables
+
+echo "Your name is {$name} and you'r {$age} years old<br>";
