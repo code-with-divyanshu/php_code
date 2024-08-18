2858c85872c6e4145855533991cb3a1b5c7756e7
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-08-18 20:14:18 +0530

    Operators in Php

---
+++ 03_operators.php
@@ -0,0 +1,39 @@
+<?php
+// Arithematic Operators
+
+$x = 36;
+$y = 24;
+$add = $x + $y;
+$sub = $x - $y;
+$multi = $x * $y;
+$divide = $x / $y;
+$reme = $x % $y;
+
+
+echo $add; // 60
+echo "<br>";
+echo $sub; // 12
+echo "<br>";
+echo $multi; // 864
+echo "<br>";
+echo $divide; // 1.5
+echo "<br>";
+echo $reme; // 12
+echo "<br>";
+
+// Increment /Decrement Operators
+
+$counter = 0;
+
+// Increment
+$counter++;
+
+echo $counter;  // 1 
+echo "<br>";
+
+
+// Decrement
+$counter--;
+
+echo $counter;  // 0 
+echo "<br>";
