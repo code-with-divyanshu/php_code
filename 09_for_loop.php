d0564f1e31d1125f128531e597aff99a7361654b
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-08-20 20:02:21 +0530

    Iteration in Php

---
+++ 09_for_loop.php
@@ -0,0 +1,13 @@
+<?php
+
+for ($i = 0; $i < 5; $i++) {
+    echo "Hello Divyanshu <br>";
+}
+
+for ($i = 1; $i <= 10; $i++) {
+    echo   "{$i}<br>";
+}
+
+for ($i = 20; $i > 0; $i--) {
+    echo   "{$i}<br>";
+}
---
+++ 10_while_loop.php
@@ -0,0 +1,17 @@
+<?php
+
+$counter = 0;
+
+while ($counter <= 10) {
+    echo "{$counter} <br>";
+    $counter++;
+}
+
+echo "<br>";
+
+
+$count = 10;
+while ($count > 0) {
+    echo "{$count} <br>";
+    $count--;
+}
